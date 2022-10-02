bed935aab84cb5cfb30e564a1ec8553aeb07e8ad
nagamaninaga <nagadj007@gmail.com>
2022-10-03 03:49:15 +0530

    filename

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Home from './Components/Home';
+import Nav from './Components/Nav';
+
+
+function App() {
+  return <>
+    <Nav/>
+   <Home/>
+
+
+  </>
+    
+}
+
+export default App;
